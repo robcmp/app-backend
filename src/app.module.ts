125725d61557d1ea7b8e77cebf21d64c9a6c2477
Roberto Mendoza <roberto.cmp16@gmail.com>
2023-01-06 03:04:07 -0300

    modified line to see NestJS own logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,36 @@
 import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongoDbModule } from './services/database/database.service';
 import { JwtModule } from '@nestjs/jwt';
 import { SECRET as secret } from './config/enviroment';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path/posix';
 import { isAuthenticated } from './middleware/auth.middleware';
 import { TaskController } from './task/task.controller';
+import { UserService } from './user/user.service';
+import { TaskService } from './task/task.service';
+import { UserController } from './user/user.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './model/user.schema';
+import { TaskSchema } from './model/task.schema';
+import * as secrets from './config/enviroment';
 
 @Module({
   imports: [
-    MongoDbModule,
+    MongooseModule.forRoot(secrets.DB_URL),
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+    MongooseModule.forFeature([{ name: 'Task', schema: TaskSchema }]),
     JwtModule.register({
       secret,
       signOptions: { expiresIn: '2h' },
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UserController, TaskController],
+  providers: [AppService, UserService, TaskService],
 })
-
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(isAuthenticated)
-      .exclude(
-        { path: 'api/v1/task/:id', method: RequestMethod.GET }
-      )
+      .exclude({ path: 'api/v1/task/:id', method: RequestMethod.GET })
       .forRoutes(TaskController);
   }
 }
