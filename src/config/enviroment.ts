f8bac4c27eb6332a76cae622d9342e9883fd5786
Roberto Mendoza <roberto.cmp16@gmail.com>
2023-01-06 00:04:38 -0300

    added variables to read secrets and function to validate them

--- src/config/enviroment.ts
+++ src/config/enviroment.ts
@@ -1,2 +1,19 @@
 require('dotenv').config();
 import { Logger } from '@nestjs/common';
+
+export const APP_PORT               : string = process.env.BLR_APP_PORT;
+export const ENVIRONMENT            : string = process.env.BLR_ENVIRONMENT;
+export const DB_URL                 : string = process.env.BLR_DB_URL;
+
+export async function validate(){
+    const logger = new Logger(); 
+    
+    let check : boolean = true;  
+    for(let k of Object.keys(this).filter(x => x != 'validate')){
+        if(!this[k]){
+            logger.error(`El secreto [${k}] no existe`)
+            check = false
+        }
+    }      
+    return check;
+}
