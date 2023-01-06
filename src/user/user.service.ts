e57a812c4ce7b3f96adf24bf0159c78285ab0038
Roberto Mendoza <roberto.cmp16@gmail.com>
2023-01-06 03:01:32 -0300

    fix: modified value in InjectModel due to errors

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,15 @@
 import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
 import { User, UserDocument } from '../model/user.schema';
+import { Model } from 'mongoose';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UserService {
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  constructor(
+    @InjectModel('User') private userModel: Model<UserDocument>,
+  ) {}
 
   async signup(user: User): Promise<User> {
     const salt = await bcrypt.genSalt();
