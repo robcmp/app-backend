8badca95a8ae16f47b7df7021e5cf38064d59613
Roberto Mendoza <roberto.cmp16@gmail.com>
2023-01-06 01:53:02 -0300

    added task controller to handle task actions in API

---
+++ src/task/task.controller.ts
@@ -0,0 +1,28 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Req,
+  Res,
+  Query,
+} from '@nestjs/common';
+import { Task } from '../model/task.schema';
+import { TaskService } from './task.service';
+
+@Controller('/api/v1')
+export class TaskController {
+  constructor(private readonly taskService: TaskService) {}
+
+  @Post()
+  async createTask(@Res() response, @Body() task: Task) {
+    const newTask = await this.taskService.createTask(task);
+    return response.status(HttpStatus.CREATED).json({
+      newTask,
+    });
+  }
+}
