aae24ee42ae8b299076978de21820285196a630a
Roberto Mendoza <roberto.cmp16@gmail.com>
2023-01-06 00:59:39 -0300

    added module to handle jwt auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongoDbModule } from './services/database/database.service';
+import { JwtModule } from '@nestjs/jwt';
+import { SECRET as secret } from './config/enviroment';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path/posix';
 
 @Module({
-  imports: [MongoDbModule],
+  imports: [
+    MongoDbModule,
+    JwtModule.register({
+      secret,
+      signOptions: { expiresIn: '2h' },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
