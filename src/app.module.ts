4aaa2abf603dc8db00d65d0ede3ba949efd3711a
Roberto Mendoza <roberto.cmp16@gmail.com>
2023-01-06 00:11:21 -0300

    added db module to be used

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongoDbModule } from './services/database/database.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, MongoDbModule],
 })
 export class AppModule {}
