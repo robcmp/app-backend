b544ef522311959502e35a828faa4cc8352d443d
Roberto Mendoza <roberto.cmp16@gmail.com>
2023-01-06 00:07:22 -0300

    added logger to run in all the app, added logs to check the running steps, added secrets validation to prevent errors when running the backend on deploy

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggerWinston } from './services/logger/logger.service';
+import * as secrets from './config/enviroment';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { logger: new LoggerWinston() });
+
+  const logger = new Logger();
+
+  logger.log("Realizando validaciones de secretos ...");
+
+  if(!await secrets.validate()){
+    logger.error("Error en la validacion de secretos, no es posible levantar servicio");
+    app.close();
+    return;
+  }
+  logger.log("Validaciones de secretos realizadas exitosamente");
+  
+  await app.listen(secrets.APP_PORT);
+  logger.log(`Servidor escuchando sobre el puerto  [${secrets.APP_PORT}]`);
 }
 bootstrap();
